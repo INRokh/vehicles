9b15104a7d2b177120f12fb9a26795949eb3316c
Nina <44559015+INRokh@users.noreply.github.com>
2020-09-16 23:55:50 +1000

    Use Spring Data REST

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,15 +1,20 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
 import java.math.BigDecimal;
+import javax.persistence.Id;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
+    @Id
+    private Long vehicleId;
 
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
+
 
     public Price() {
     }
