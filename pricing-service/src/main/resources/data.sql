77b4d7a4b0004a689513395a7c08995be4da9bf8
Nina <44559015+INRokh@users.noreply.github.com>
2020-10-15 18:57:49 +1100

    Price fix

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (ID, vehicle_Id, currency, price) VALUES(1, 1, 'USD', '42000');
+INSERT INTO price (ID, vehicle_Id, currency, price) VALUES(2, 2, 'RMB', '160000');
+INSERT INTO price (ID, vehicle_Id, currency, price) VALUES(3, 3, 'GPD', '98076');
